e926e67fa1a8504cec9ae2f31c8fc4bae115a27b
CypherKWeb <161012129+CypherKWeb@users.noreply.github.com>
2024-12-11 00:27:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,7 +124,7 @@ async def not_joined(client: Client, message: Message):
         [
             InlineKeyboardButton(
                 "⛩️ Ｃʜᴀɴɴᴇʟ 1 ⛩️",
-                url = f"https://t.me/Anime_Cypher"),
+                url = f"https://t.me/UnVerb"),
             InlineKeyboardButton(
                 "⛩️ Ｃʜᴀɴɴᴇʟ 2 ⛩️",
                 url = client.invitelink)
